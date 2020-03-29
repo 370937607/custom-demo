775b99996f0fa6ad5844de866ee881278502d8bc
donggy <370937607@qq.com>
2020-03-29 20:30:42 +0800

    Initial commit

---
+++ src/main/java/com/example/customdemo/repository/MessageRepository.java
@@ -0,0 +1,20 @@
+package com.example.customdemo.repository;
+
+import com.example.customdemo.pojo.Message;
+
+import java.util.List;
+
+
+public interface MessageRepository {
+    List<Message> findAll();
+
+    Message findOne(Long id);
+
+    Message save(Message message);
+
+    void delete(Long id);
+
+    Message update(Message message);
+
+    Message updateText(Message message);
+}
